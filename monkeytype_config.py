14836fe03731043ea92470bfa8c5fcd3fb6268ee
MichAdebayo <adebayomichael4@gmail.com>
2025-05-14 14:54:21 +0200

    "git action attempt 15"

--- monkeytype_config.py
+++ monkeytype_config.py
@@ -2,11 +2,10 @@
 from monkeytype_config import DefaultConfig
 
 
-
 class Config(DefaultConfig):
     # Ignore files you don’t care about
     exclude = ('.venv/', '__pycache__/', 'migrations/', '.vscode/', '.pytest_cache/', '.coverage', 'tests/', 'admin.py')
 
     # Optional: control output formatting
     formatter = 'black'
-    
+
