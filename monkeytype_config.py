56331539292eb627baa416ad21a74523acbe0def
MichAdebayo <adebayomichael4@gmail.com>
2025-05-14 14:51:54 +0200

    "refactored views.py"

--- monkeytype_config.py
+++ monkeytype_config.py
@@ -1,9 +1,12 @@
 # monkeytype_config.py
 from monkeytype_config import DefaultConfig
 
+
+
 class Config(DefaultConfig):
     # Ignore files you don’t care about
     exclude = ('.venv/', '__pycache__/', 'migrations/', '.vscode/', '.pytest_cache/', '.coverage', 'tests/', 'admin.py')
 
     # Optional: control output formatting
     formatter = 'black'
+    
