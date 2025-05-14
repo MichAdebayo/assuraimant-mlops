40f551282c72cf34690dbf8ff43d9cba6aa22d50
MichAdebayo <adebayomichael4@gmail.com>
2025-05-14 14:07:25 +0200

    "edit git action file"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="assuraimant_ci_cd",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
