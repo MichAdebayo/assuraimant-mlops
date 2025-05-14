3349d6f334d90552ac3322d441617e9ffd7ffc2b
MichAdebayo <adebayomichael4@gmail.com>
2025-05-14 13:23:07 +0200

    "resolving github actions issues"

--- brief_app/insurance_app/static/js/admin/flatpickr-init.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    flatpickr("#id_date", {
-        enableTime: false,
-        dateFormat: "Y-m-d",  // Format for the date
-    });
-});
