233bd7cd60c02e166d90a191d32350fccc4e4cd4
MichAdebayo <adebayomichael4@gmail.com>
2025-05-14 23:59:28 +0200

    "debugging docker compose"

--- src/ci_cd/shell/build_docker.sh
+++ src/ci_cd/shell/build_docker.sh
@@ -6,3 +6,7 @@ set -e
 # Build Docker Compose image
 echo "Building Docker Compose image..."
 docker-compose -f ./src/ci_cd/docker/docker-compose.yml build
+
+# Tag the Docker image
+echo "Tagging Docker image..."
+docker tag ghcr.io/${REPO_LC}/assuraimant-web-app:latest $IMAGE_NAME:$IMAGE_TAG
