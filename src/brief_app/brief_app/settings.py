df3098fb36dd487b062a2e5a9315a5e8a3bd6b27
MichAdebayo <adebayomichael4@gmail.com>
2025-05-16 00:17:41 +0200

    "cd debugging"

--- src/brief_app/brief_app/settings.py
+++ src/brief_app/brief_app/settings.py
@@ -57,7 +57,7 @@ TAILWIND_APP_NAME = "theme"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware", 
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
