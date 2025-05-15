dc197ae4fe038592550128d2e216aa78ea54c585
MichAdebayo <adebayomichael4@gmail.com>
2025-05-15 09:26:06 +0200

    "ci resolution in progress"

--- src/ci_cd/shell/build_docker.sh
+++ src/ci_cd/shell/build_docker.sh
@@ -3,10 +3,8 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Build Docker Compose image
-echo "Building Docker Compose image..."
-docker-compose -f ./src/ci_cd/docker/docker-compose.yml build
+export IMAGE_TAG="$1"
 
-# # Tag the Docker image
-# echo "Tagging Docker image..."
-# docker tag ghcr.io/${REPO_LC}/assuraimant-web-app:latest $IMAGE_NAME:$IMAGE_TAG
+# Build Docker Compose image
+echo "Building Docker Compose image with IMAGE_TAG=$IMAGE_TAG..."
+docker-compose -f [./src/ci_cd/docker/docker-compose.yml](VALID_FILE) build
