a57e38075a42c07e306c56d78d6b0468904c5dd6
MichAdebayo <adebayomichael4@gmail.com>
2025-05-15 09:30:25 +0200

    "ci resolution in progress"

--- src/ci_cd/shell/build_docker.sh
+++ src/ci_cd/shell/build_docker.sh
@@ -7,4 +7,4 @@ export IMAGE_TAG="$1"
 
 # Build Docker Compose image
 echo "Building Docker Compose image with IMAGE_TAG=$IMAGE_TAG..."
-docker-compose -f [./src/ci_cd/docker/docker-compose.yml](VALID_FILE) build
+docker-compose -f ./src/ci_cd/docker/docker-compose.yml build
