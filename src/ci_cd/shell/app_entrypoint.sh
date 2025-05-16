1f151154a0b88fa8645d09b5882de7ec3e243c0e
MichAdebayo <adebayomichael4@gmail.com>
2025-05-16 14:21:32 +0200

    conducting final autodeployment test

--- src/ci_cd/shell/app_entrypoint.sh
+++ src/ci_cd/shell/app_entrypoint.sh
@@ -10,12 +10,6 @@ until python manage.py showmigrations &>/dev/null; do
   sleep 3
 done
 
-# echo "Waiting for database to be ready..."
-# until pg_isready -h db -p 5432 -U postgres; do
-#   echo "Database not ready, retrying in 3s..."
-#   sleep 3
-# done
-
 echo "Database is ready. Running database migrations..."
 if ! python manage.py migrate; then
   echo "❌ Database migrations failed"
@@ -30,26 +24,3 @@ fi
 
 echo "Starting the application on port $GUNICORN_PORT..."
 exec gunicorn brief_app.wsgi:application --bind 0.0.0.0:$GUNICORN_PORT --access-logfile -
-
-
-
-# #!/bin/bash
-
-# set -e
-
-# GUNICORN_PORT=${GUNICORN_PORT:-8000}
-
-# echo "Waiting for database to be ready..."
-# until python manage.py showmigrations &>/dev/null; do
-#   echo "Database not ready, retrying in 3s..."
-#   sleep 3
-# done
-
-# echo "Running database migrations..."
-# python manage.py migrate
-
-# echo "Collecting static files..."
-# python manage.py collectstatic --noinput || { echo "❌ Collectstatic failed"; exit 1; }
-
-# echo "Starting the application on port $GUNICORN_PORT..."
-# gunicorn brief_app.wsgi:application --bind 0.0.0.0:$GUNICORN_PORT --access-logfile -
--- src/ci_cd/shell/deploy.sh
+++ src/ci_cd/shell/deploy.sh
@@ -7,14 +7,6 @@ set -e
 echo "Logging into GitHub Container Registry..."
 echo "$GITHUB_TOKEN" | docker login ghcr.io -u "$GITHUB_ACTOR" --password-stdin
 
-# Debugging output
-# echo "Using REPO_LC=${REPO_LC}"
-# echo "Using IMAGE_TAG=${IMAGE_TAG}"
-# echo "Using DATABASE_URL=${DATABASE_URL}"
-# echo "Using SECRET_KEY=${SECRET_KEY}"
-# echo "Using RENDER_API_TOKEN: ${RENDER_API_TOKEN}"
-# echo "GITHUB_ACTOR is: $GITHUB_ACTOR"
-
 # Validate required environment variables
 if [[ -z "$REPO_LC" || -z "$IMAGE_TAG" || -z "$RENDER_API_TOKEN" ]]; then
   echo "❌ One or more required environment variables are not set. Exiting..."
@@ -49,9 +41,12 @@ RESPONSE=$(curl -s -w "\n%{http_code}" -X POST "https://api.render.com/v1/servic
 HTTP_STATUS=$(echo "$RESPONSE" | tail -n1)
 RESPONSE_BODY=$(echo "$RESPONSE" | head -n -1)
 
+echo "Deploying image: ghcr.io/${REPO_LC}/assuraimant-web-app:${IMAGE_TAG}"
+
 if [[ "$HTTP_STATUS" != "200" && "$HTTP_STATUS" != "201" ]]; then
   echo "❌ Deployment failed with status code $HTTP_STATUS"
   echo "Response: $RESPONSE_BODY"
-  echo "✅ Deployment to Render completed successfully!"
   exit 1
 fi
+
+echo "✅ Deployment to Render completed successfully!"
